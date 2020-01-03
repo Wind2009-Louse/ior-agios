b303fc7c91fca4ffe31336f093be1e570546605e
Wind2009-Louse <wind.jackey@qq.com>
2020-01-03 15:58:04 +0800

    Revert "Create mpi_comm_nozero"
    This reverts commit b0e28b83cc3a2731c9c109b4998da09ec0b9ca75.

--- src/utilities.c
+++ src/utilities.c
@@ -60,7 +60,6 @@ int      tasksPerNode = 0;           /* tasks per node */
 int      verbose = VERBOSE_0;        /* verbose output */
 MPI_Comm testComm;
 MPI_Comm mpi_comm_world;
-MPI_Comm mpi_comm_nozero;
 FILE * out_logfile;
 FILE * out_resultfile;
 enum OutputFormat_t outputFormat;
